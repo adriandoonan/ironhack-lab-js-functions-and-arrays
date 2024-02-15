7d0ca4f37b7680622d88faa863a6fac67aca084d
Adrian Doonan <adriandoonan@gmail.com>
2024-02-15 13:54:01 +0100

    use arrow functions

--- src/functions-and-arrays.js
+++ src/functions-and-arrays.js
@@ -1,7 +1,5 @@
 // Iteration 1 | Find the Maximum
-function maxOfTwoNumbers(num1,num2) {
-    return num1 >= num2 ? num1 : num2
-}
+const maxOfTwoNumbers = (num1,num2) => num1 >= num2 ? num1 : num2
 
 
 
@@ -9,9 +7,9 @@ function maxOfTwoNumbers(num1,num2) {
 // Iteration 2 | Find the Longest Word
 const words = ["mystery", "brother", "aviator", "crocodile", "pearl", "orchard", "crackpot"];
 
-function findLongestWord(arrayOfWords) {
-    return arrayOfWords.reduce((a,b) => a.length >= b.length ? a : b, '') || 0
-}
+const findLongestWord = arrayOfWords => 
+    arrayOfWords.reduce((a,b) => a.length >= b.length ? a : b, '') || 0
+
 
 
 
@@ -19,23 +17,21 @@ function findLongestWord(arrayOfWords) {
 // Iteration 3 | Sum Numbers
 const numbers = [6, 12, 1, 18, 13, 16, 2, 1, 8, 10];
 
-function sumNumbers(arrayOfNumbers) {
-    return arrayOfNumbers.reduce((a,b) => a + b, 0)
-}
+const sumNumbers = arrayOfNumbers => arrayOfNumbers.reduce((a,b) => a + b, 0)
 
 
 // Iteration 4 | Numbers Average
 const numbers2 = [2, 6, 9, 10, 7, 4, 1, 9];
 
-function averageNumbers(arrayOfNumbers) {
-    return sumNumbers(arrayOfNumbers) / arrayOfNumbers.length || 0
-}
+const averageNumbers = arrayOfNumbers =>
+    sumNumbers(arrayOfNumbers) / arrayOfNumbers.length || 0
+
 
 
 
 // Iteration 5 | Find Elements
 const words2 = ["machine", "subset", "trouble", "starting", "matter", "eating", "truth", "disobedience"];
 
-function doesWordExist(arrayOfWords, searchString) {
-    return !arrayOfWords.length ? null : arrayOfWords.some((word) => word === searchString)
-}
+const doesWordExist = (arrayOfWords, searchString) =>
+    !arrayOfWords.length ? null : arrayOfWords.some((word) => word === searchString)
+
